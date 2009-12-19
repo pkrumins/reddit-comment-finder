fd8e745c01f69e10b63e392c74fbf908c095d610
Peteris Krumins <peteris.krumins@gmail.com>
2009-12-19 22:14:44 +0200

    initial reddit comment finder import

---
+++ readme.txt
@@ -0,0 +1,75 @@
+This is the Reddit Comment Finder program. It's written in Python and it finds
+as many comments as it can for a given person on Reddit.
+
+It was written by Peteris Krumins (peter@catonmat.net).
+His blog is at http://www.catonmat.net  --  good coders code, great reuse.
+
+The code is licensed under the GNU GPL license.
+
+------------------------------------------------------------------------------
+
+This program goes through all the person's Reddit comments at
+http://www.reddit.com/user/<nickname>/comments/ and prints them out.
+
+The usage is as following:
+
+    $ ./reddit_comments.py <username> [number of comment pages]
+
+The default is to get 1 page of comments for reddit user "username".
+If you specify magic argument -1 for [number of comment pages], it will get
+all the pages.
+
+Here is an example. It gets 1 page of comments for "redditor":
+
+    $ ./reddit_comments.py redditor
+
+    Post title: I just guessed the password to this account. Am I bad?
+    Comment date:  Thu Dec 17 17:12:20 2009
+    Comment score: 24 (26 upvotes, 2 downvotes)
+    Comment URL:   http://www.reddit.com/comments/afmwd/xyzzy/c0hchmq
+    Comment:
+    *angry glare*
+    --------------------------------------------------------------------------
+    ...
+
+
+You may also import reddit_comments as Python module:
+
+    >>> import reddit_comments as rc
+    >>>
+    >>> comments = rc.get_comments("kn0thing", 2)
+    >>> len(comments)
+    50
+    >>> rc.print_comments(comments)
+    >>> ...
+
+This example imports "reddit_comments" module as "rc", then it calls the
+get_comments() function, that takes the username and number of pages of
+comments to get, and returns a list of Comment objects. Next print_comments()
+function is called that prints all the comments in the format above.
+
+Each Comment object has the following attributes:
+
+    * author       - username of author
+    * comment      - comment plain text
+    * upvotes      - number of upvotes
+    * downvotes    - number of downvotes
+    * score        - upvotes-downvotes
+    * time_utc     - utc time in unix tiem format
+    * time_human   - human readable time
+    * link_title   - link title
+    * link_id      - id of the link
+    * comment_id   - id of the comment
+    * url          - url to the comment
+
+You may write your own print_comments() function that, for example, would
+output the comments as HTML.
+
+------------------------------------------------------------------------------
+
+Have fun finding your old comments. ;)
+
+Sincerely,
+Peteris Krumins
+http://www.catonmat.net
+
---
+++ reddit_comments.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+#
+# Peteris Krumins (peter@catonmat.net)
+# http://www.catonmat.net  --  good coders code, great reuse
+#
+# Finds all comments by a user, outputs them to stdout.
+#
+
+try:    import json
+except: import simplejson as json
+from itertools import count
+import urllib2
+import socket
+import time
+import sys
+
+COMMENTS_URL = "http://www.reddit.com/user/%s/comments/.json"
+COMMENT_URL  = "http://www.reddit.com/comments/%(link_id)s/xyzzy/%(comment_id)s"
+
+class Comment(object):
+    """
+    Encapsulates the information about a comment
+
+    After the object has been constructed, it contains the following attrs:
+    * author       - username of author
+    * comment      - comment plain text
+    * upvotes      - number of upvotes
+    * downvotes    - number of downvotes
+    * score        - upvotes-downvotes
+    * time_utc     - utc time in unix tiem format
+    * time_human   - human readable time
+    * link_title   - link title
+    * link_id      - id of the link
+    * comment_id   - id of the comment
+    * url          - url to the comment
+    """
+
+def get_comments(user, pages=1, print_callback=None):
+    """
+    Given user, gets pages of comments (25 comments per page).
+    If pages is -1, gets all the comments.
+    If print_callback is specified, calls it after each page.
+    """
+    if print_callback is None:
+        print_callback = lambda x: None
+
+    base_url = comment_url = COMMENTS_URL % user
+
+    comments = []
+    for page_nr in count(1):
+        if page_nr > pages and pages != -1:
+            break
+        print_callback(page_nr)
+        comment_page = get_page(comment_url)
+        comments.extend(extract_comments(comment_page))
+        comment_url  = get_next_page_url(comment_page, base_url)
+        if not comment_url: break
+    return comments
+
+def extract_comments(comment_page):
+    """
+    Given a JSON comment page, constructs Comment object for each comment and
+    returns a list of them.
+    """
+    comments = []
+    comment_struct = json.loads(comment_page)
+    items = comment_struct['data']['children']
+    for item in items:
+        data = item['data']
+        comment = Comment()
+        comment.author      = data['author']
+        comment.comment     = data['body']
+        comment.time_utc    = data['created_utc']
+        comment.human_utc   = time.ctime(comment.time_utc)
+        comment.link_title  = data['link_title']
+        comment.link_id     = data['link_id'].replace("t3_","")
+        comment.comment_id  = data['name'].replace("t1_","")
+        comment.upvotes     = data['ups']
+        comment.downvotes   = data['downs']
+        comment.score       = comment.upvotes-comment.downvotes
+        comment.url         = COMMENT_URL % {
+                'link_id': comment.link_id,
+                'comment_id': comment.comment_id 
+        }
+        comments.append(comment)
+    return comments
+
+def get_next_page_url(comment_page, base_url):
+    """
+    Given a comment page, finds the URL to the next page.
+    """
+    comment_struct = json.loads(comment_page)
+    after = comment_struct['data']['after']
+    if after: return base_url + '?after=' + after
+
+def get_page(url, timeout=10):
+    """
+    Gets and returns a web page at url with timeout 'timeout'.
+    """
+
+    old_timeout = socket.setdefaulttimeout(timeout)
+
+    request = urllib2.Request(url)
+    request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')
+
+    try:
+        response = urllib2.urlopen(request)
+        content = response.read()
+    finally:
+        socket.setdefaulttimeout(old_timeout)
+
+    return content
+
+def get_all_comments(user, print_callback):
+    """ Gets all comments for user """
+    return get_comments(user, -1, print_callback)
+
+def print_comments(comments):
+    """ Prints the given list of Comment objects """
+    for comment in comments:
+        print "Post title: " + comment.link_title.encode('utf-8')
+        print "Comment date: " + comment.human_utc
+        print "Comment score: %d (%d upvotes, %d downvotes)" % (
+                comment.score, comment.upvotes, comment.downvotes
+        )
+        print "Comment URL: " + comment.url
+        print 'Comment:'
+        print comment.comment.encode('utf-8')
+        print '-'*78
+        print
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if not args:
+        print >>sys.stderr, "Usage: %s <username>" % sys.argv[0]
+        sys.exit(1)
+
+    user = args[0]
+
+    def callback(page_nr):
+        print >>sys.stderr, "Getting page %d of %s's comments" % (page_nr, user)
+
+    comments = get_all_comments(user, callback)
+    print >>sys.stderr, "Got %d comments" % len(comments)
+    print_comments(comments)
+
